c69822ee64194aa5d487ee8a07cebe1c7d12e1da
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-13 14:52:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your code here
+const gameContainer = document.getElementById('gameContainer');
+const gridSize = 20; // The number of pixels in each row and column
+const snake = [{ row: 20, col: 1 }]; // Initial position of the snake
+let food = { row: Math.floor(Math.random() * gridSize), col: Math.floor(Math.random() * gridSize) }; // Initial position of the food
+let direction = 'right'; // Initial direction of the snake
+
+function update() {
+  // Update the snake's position based on the current direction
+
+  // Add code to move the snake according to the direction
+
+  // Check if the snake has eaten the food and update the snake's length
+
+  // Update the game display based on the snake's position and food
+}
+
+function draw() {
+  // Clear the game container
+
+  // Draw the snake
+
+  // Draw the food
+
+  // Update the score on the scoreboard
+}
+
+setInterval(() => {
+  update();
+  draw();
+}, 100); // Update the game every 100ms
