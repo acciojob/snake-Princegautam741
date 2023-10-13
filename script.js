6df3eb4c91aead96845b2a881d3df7ba13f1c52f
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-13 14:55:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
 const gameContainer = document.getElementById('gameContainer');
-const gridSize = 20; // The number of pixels in each row and column
-const snake = [{ row: 20, col: 1 }]; // Initial position of the snake
+const gridSize = 40;
+let snake = [{ row: 20, col: 1 }]; // Initial position of the snake
 let food = { row: Math.floor(Math.random() * gridSize), col: Math.floor(Math.random() * gridSize) }; // Initial position of the food
 let direction = 'right'; // Initial direction of the snake
 
 function update() {
   // Update the snake's position based on the current direction
+  let head = { ...snake[0] };
 
-  // Add code to move the snake according to the direction
+  // Move the snake
+  switch (direction) {
+    case 'up':
+      head.row--;
+      break;
+    case 'down':
+      head.row++;
+      break;
+    case 'left':
+      head.col--;
+      break;
+    case 'right':
+      head.col++;
+      break;
+  }
 
-  // Check if the snake has eaten the food and update the snake's length
+  // Add the new head
+  snake.unshift(head);
 
-  // Update the game display based on the snake's position and food
+  // Check if the snake has eaten the food
+  if (head.row === food.row && head.col === food.col) {
+    // Generate new food
+    food = { row: Math.floor(Math.random() * gridSize), col: Math.floor(Math.random() * gridSize) };
+  } else {
+    // Remove the tail of the snake
+    snake.pop();
+  }
+
+  // Update the game display
+  draw();
 }
 
 function draw() {
   // Clear the game container
+  gameContainer.innerHTML = '';
 
   // Draw the snake
+  snake.forEach(segment => {
+    const snakePixel = document.createElement('div');
+    snakePixel.classList.add('snakeBodyPixel');
+    snakePixel.style.gridRowStart = segment.row;
+    snakePixel.style.gridColumnStart = segment.col;
+    gameContainer.appendChild(snakePixel);
+  });
 
   // Draw the food
-
-  // Update the score on the scoreboard
+  const foodPixel = document.createElement('div');
+  foodPixel.classList.add('food');
+  foodPixel.style.gridRowStart = food.row;
+  foodPixel.style.gridColumnStart = food.col;
+  gameContainer.appendChild(foodPixel);
 }
 
 setInterval(() => {
   update();
-  draw();
 }, 100); // Update the game every 100ms
